2c2ea34c5bd8976936bfd3eb79dd457d959d67fa
Adam <adampugh@zoho.com>
2021-02-18 17:36:42 +0000

    modules

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        // public: 'dist',
+    },
+    devServer: {
+        publicPath: '/dist/',
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+};
